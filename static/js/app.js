e62e995bd53e3186f8a415cdf1c8d6af7abb8805
Joey Picca <joeypicca@gmail.com>
2020-03-27 15:27:25 -0500

    Added gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -101,6 +101,44 @@ function initPlatform() {
 
       Plotly.newPlot('bubble', trace1, layout2);
 
+      //
+      // *** Gauge Chart ***
+      //
+
+      var data3 = [
+        {
+          type: "indicator",
+          mode: "gauge+number",
+          value: metaData[0].wfreq,
+          title: { text: "Wash Frequency", font: { size: 24 } },
+          //delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+          gauge: {
+            axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" },
+            bar: { visible: false, color: "darkblue" },
+            bgcolor: "white",
+            borderwidth: 2,
+            bordercolor: "gray",
+            steps: [
+              { range: [0, 2], color: "#eff3ff" },
+              { range: [2, 4], color: "#bdd7e7" },
+              { range: [4, 6], color: "#6baed6" },
+              { range: [6, 8], color: "#3182bd" },
+              { range: [8, 10], color: "#08519c" }
+            ]
+          }
+        }
+      ];
+      
+      var layout3 = {
+        //width: 400,
+        //height: 300,
+        margin: { t: 25, r: 25, l: 25, b: 25 },
+        paper_bgcolor: "white",
+        font: { color: "darkblue", family: "Arial" }
+      };
+      
+      Plotly.newPlot('gauge', data3, layout3);
+
 };
 
 function optionChanged(testSubject) {
@@ -150,5 +188,13 @@ function optionChanged(testSubject) {
     Plotly.restyle("bubble", "text", [text2]);
     Plotly.restyle("bubble", "marker.color", [x2]);
     Plotly.restyle("bubble", "marker.size", [y2]);
+
+    //
+    // *** Gauge Chart ***
+    //
+
+    let newVal = metaData[0].wfreq;
+
+    Plotly.restyle("gauge", "value", [newVal]);
 
 };
